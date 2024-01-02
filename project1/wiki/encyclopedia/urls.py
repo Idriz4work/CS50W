cf87568fab09ac56d0d6a93ba4e983ddf1091a48
Idriz Atilla <136346643+Idriz4work@users.noreply.github.com>
2024-01-02 12:43:25 +0000

    change

--- project1/wiki/encyclopedia/urls.py
+++ project1/wiki/encyclopedia/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
+from .util import list_entries
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("", views.entry, name="entry"),
+    path("", views.create_new, name="create"),
+    path("", views.random, name="random")
 ]
--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.http import HttpResponse
+from .util import list_entries
 
 from . import util
 
@@ -8,3 +12,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def view(request, ):
+
+    return HttpResponse("Hello, world!")
+
+
+def create_new(request):
+    return render(request,"encyclopedia/create.html",{
+        "new_page": util.list_entries()
+    })
+
+def random(request):
+    return render(request,"encyclopedia/random.html",{
+        "random_page": util.list_entries()
+    })
