e33c91a295b3f0a3e872c508dd754f47956dfaaa
Idriz Atilla <136346643+Idriz4work@users.noreply.github.com>
2024-01-05 15:29:37 +0000

    #

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -2,14 +2,13 @@
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .util import list_entries
 from django.db import connection
 from . import util
 from sqlalchemy import create_engine, MetaData
 
-# You need to replace "SQL" with "create_engine" and create a MetaData object
-db = create_engine("sqlite:///db.sqlite3")
+# Assuming you have already defined your Django app instance in the 'app' variable
+
 
 def index(request):
     if request.method == "POST":
@@ -17,38 +16,35 @@ def index(request):
             "entries": util.list_entries()
         })
     else:
-        title = db.execute("SELECT title from saved_pages") 
-        if title != 0:
+        title = connection.cursor()
+        title.execute("SELECT title FROM saved_pages")
+        title_result = title.fetchone()
+
+        if title_result:
             return render(request, "encyclopedia/index.html", {
-            "ent": util.get_entry(title)
-        })
+                "ent": util.get_entry(title_result[0])
+            })
         else:
             return render(request, "encyclopedia/index.html")
 
-
 def create_new(request):
     if request.method == "POST":
-        # Assuming you want to get the user_id from the session, adjust as needed
         user_id = request.session.get('user_id', None)
-        
-        # Get data from the POST request
         title = request.POST.get("title", "")
         text = request.POST.get("textar", "")
 
         if user_id not in request.session:
-            # If not, create a new list
             request.session["user_id"] = []
 
         with connection.cursor() as cursor:
-            # Replace your current cursor.execute line with this
-            cursor.execute("INSERT INTO saved_pages (user_id, title, page_text) VALUES (?, ?, ?)", (user_id, title, text))
-            # Save the new page to the database
+            cursor.execute("INSERT INTO saved_pages (user_id, title, page_text) VALUES (%s, %s, %s)", (user_id, title, text))
 
         return render(request, "encyclopedia/create.html", {
             "user_id": request.session["user_id"]
-        })  # Redirect to a specific page after saving
+        })
     else:
         return render(request, "encyclopedia/create.html")
+
 
 def random(request):
     user_id = session(id)
