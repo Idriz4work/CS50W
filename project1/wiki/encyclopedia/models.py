13dd480327fd3166df738105a526d41ebee1b8b9
Idriz Atilla <136346643+Idriz4work@users.noreply.github.com>
2024-01-05 16:08:58 +0000

    first migration

--- project1/wiki/encyclopedia/models.py
+++ project1/wiki/encyclopedia/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class saved_pages(models.Model):
+    title = models.CharField(max_length=64)
+    text_page = models.TextField()
+    user_id = models.IntegerField()
