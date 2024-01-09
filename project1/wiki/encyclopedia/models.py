e015d9460510c879aa393e0894e56398f48716c1
Idriz Atilla <136346643+Idriz4work@users.noreply.github.com>
2024-01-09 11:07:00 +0000

    #

--- project1/wiki/encyclopedia/models.py
+++ project1/wiki/encyclopedia/models.py
@@ -2,9 +2,8 @@
 from django.db import models
 
 class saved_pages(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.TextField(max_length=64)
     text_page = models.TextField()
-    user_id = models.IntegerField(primary_key=True)
 
     def __str__(self):
         return f"{self.user_id}: {self.title} to {self.text_page}"
