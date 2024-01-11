6f06ca9f27c63fb7d0056fcf28caff0030df7554
Idriz Atilla <136346643+Idriz4work@users.noreply.github.com>
2024-01-11 14:26:46 +0000

    #

--- project2/commerce/auctions/models.py
+++ project2/commerce/auctions/models.py
@@ -5,19 +5,32 @@ from django.db import models
 class User(AbstractUser):
     # No need to redefine username, email, and password
     user_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user_id}"
     
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     origin = models.CharField(max_length=64)
     duration = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.user}: {self.title} to {self.text_page}"
+
 class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     origin = models.CharField(max_length=64)
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user}: {self.title} to {self.text_page}"
+    
 
 class AuctionListing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     item_name = models.CharField(max_length=255)  # Adjust max length as needed
     item_count = models.IntegerField()
     price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user}: {self.item_name} and {self.item_count } to {self.price}"
