d61bc67f4b6f7ef8a89dcb5a38014ddd67c137b9
Idriz Atilla <136346643+Idriz4work@users.noreply.github.com>
2024-02-09 09:40:20 +0000

    #

--- project3/javaScript/app.jsx
+++ project3/javaScript/app.jsx
@@ -1,5 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
 
-render_about(){
-    document.querySelector('#ab');
-    return false
+    // Use buttons to toggle between views
+    document.querySelector('#about').addEventListener('click', () => render_about());
+    document.querySelector('#projects').addEventListener('click', () => render_projects());
+    document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+    document.querySelector('#compose').addEventListener('click', compose_email);
+  
+    document.querySelector('#compose-form').addEventListener('submit', submit_email)
+  
+    // By default, load the inbox
+    load_mailbox('inbox');
+    
+  });
+
+function render_about(){
+   const content = document.querySelector('.content');
+    content.innerHTML =
+    `<h1>Za real zorilla is back </h1>`
+}
+
+function render_projects(){
+
 }
